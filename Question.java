3410418de411fa6553cf4e3889f75902ca59d2d5
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-19 17:50:45 +0330

    Add constructor to Question class for initialization

--- Question.java
+++ Question.java
@@ -6,6 +6,21 @@ public class Question {
     private String opt3;
     private String opt4;
     private String answer;
+
+
+
+    
+
+
+    public Question(int id, String question, String opt1, String opt2, String opt3, String opt4, String answer) {
+        this.id = id;
+        this.question = question;
+        this.opt1 = opt1;
+        this.opt2 = opt2;
+        this.opt3 = opt3;
+        this.opt4 = opt4;
+        this.answer = answer;
+    }
     public int getId() {
         return id;
     }
