3f49121d8cafa3df174a6b59b066573b2ac0e8c4
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-19 19:30:21 +0330

    Add fly method to Car class and implement in WagonR class

--- CarMain.java
+++ CarMain.java
@@ -3,6 +3,8 @@ abstract class Car {
     // abstract: i give you the method but you should implement it your self
     public abstract void drive();
 
+    public abstract void fly();
+
     public void playMusic() {
         System.out.println("Play Music");
     }
@@ -11,13 +13,20 @@ abstract class Car {
 // this class must implement the abstract method defined by the car class
 class WagonR extends Car {
     public void drive() {
+        System.out.println("driving ");
+    }
 
+    public void fly() {
+
+        System.out.println("flying");
     }
 
 }
 
 public class CarMain {
     public static void main(String[] args) {
-
+        WagonR wagonR = new WagonR();
+        wagonR.drive();
+        wagonR.fly();
     }
 }
