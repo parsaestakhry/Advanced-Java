5c20d5c62a33279d620d63ce571188b73112004e
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-19 19:25:37 +0330

    Refactor Car class to be abstract and implement drive method in WagonR class

--- CarMain.java
+++ CarMain.java
@@ -1,17 +1,23 @@
-class Car {
-    public void drive() {
-        System.out.println("Driving a car ");
-    }
-    
+abstract class Car {
+
+    // abstract: i give you the method but you should implement it your self
+    public abstract void drive();
+
     public void playMusic() {
         System.out.println("Play Music");
     }
 }
 
+// this class must implement the abstract method defined by the car class
+class WagonR extends Car {
+    public void drive() {
+
+    }
+
+}
+
 public class CarMain {
     public static void main(String[] args) {
-        Car car = new Car();
-        car.drive();
-        car.playMusic();
+
     }
 }
